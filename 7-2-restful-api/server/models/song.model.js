653817a594936baf480a79a2a5c41f85686d5427
Wajza <s202262140@kfupm.edu.sa>
2026-04-15 11:12:25 +0300

    server.js &  song.model.js-completed

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -2,4 +2,36 @@ import mongoose from "mongoose";
 
 // db schema
 
+const songSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  artist: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  album: {
+    type: String,
+    trim: true
+  },
+  year: {
+    type: Number,
+    min: 1900,
+    max: new Date().getFullYear()
+  },
+  genre: {
+    type: String,
+    trim: true
+  },
+  duration: {
+    type: Number,
+    min: 0
+  }
+}, {
+  timestamps: true
+});
+
 export const Song = mongoose.model("Song", songSchema);
