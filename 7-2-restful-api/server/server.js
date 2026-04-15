9f0d8dfb54e4a82284ce65042fca7d4fa63fb30f
Wajza <s202262140@kfupm.edu.sa>
2026-04-15 12:00:49 +0300

    TODO6

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -62,5 +62,14 @@ app.put("/api/songs/:id", async (req, res) => {
 });
 
 // /api/songs/:id (Delete song)
+app.delete("/api/songs/:id", async (req, res) => {
+  try {
+    const d = await Song.findByIdAndDelete(req.params.id);
+    if (!d) return res.status(404).json({ message: "Song not found" });
+    res.status(204).end();
+  } catch (err) {
+    res.status(400).json({ message: err.message || "Failed to delete song" });}
+});
+
 
 app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
